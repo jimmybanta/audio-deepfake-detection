3e27683a107c556effa5cafef1dc25461bb91af7
jimmybanta <jimmy.banta24@gmail.com>
2024-12-02 19:56:46 -0500

    completed data input

--- README.md
+++ README.md
@@ -1,10 +1,54 @@
 # Audio Deepfake Detection
 
+
+## Data Collection
+
+Using the 'In-the-Wild' dataset of audio deepfake data from kaggle -- [link](https://www.kaggle.com/datasets/abdallamohamed312/in-the-wild-dataset/data)
+
+See 'data/data_meta.csv' for data on the samples and their labels.
+
+Stats:
+- 31,779 total audio samples of varying lengths, with a sample rate of 16kHz
+  - 19,963 real
+  - 11,816 fake
+  - An average length of 4.28 seconds
+- 54 total speakers
+  - these are real people, and the dataset includes both real and spoofed audio of them speaking
+
+Splitting data:
+
+
+Evaluation:
+- One speaker (Alec Guiness) was removed - he will be our final evaluation set
+- This way, the model will have never heard any of his audio before
+  - Alec Guiness:
+    - 1907 real clips
+    - 1718 fake clips
+
+  - This leaves (for our train and test sets):
+    - 28,154 clips
+      - 18,056 real
+      - 10,098 fake
+
+Train & Test:
+- I want to split them up so that any individual speaker is only in one set
+- I'll try to split it 80/20
+- Train:
+  - 20,738 clips (73%)
+    - 12,745 real (61%)
+    - 7,993 fake (39%)
+- Test:
+  - 7,416 clips (27%)
+    - 5,311 real (71%)
+    - 2,105 fake (29%)
+
+
+
+
 ## To Do:
 
 - [x] Collect data
-  - 'In-the-Wild' Audo deepfake data from kaggle -- [link](https://www.kaggle.com/datasets/abdallamohamed312/in-the-wild-dataset/data)
-  - see 'data_meta.csv' for a list of all audio samples and their labels
+- [x] split data
 - [ ] Data pre-processing
 - [ ] Build CNN
 - [ ] CNN Training
---
+++ data/import_data.py
@@ -0,0 +1,138 @@
+''' Contains functions for importing data from file into pytorch. '''
+
+import os
+import pandas as pd
+from dotenv import load_dotenv
+
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import torchaudio
+
+load_dotenv()
+
+
+# setup the Mel Spectrogram transform
+transform = torchaudio.transforms.MelSpectrogram(16000)
+
+def pad_tensor(tensor, target_length):
+    '''
+    Given a tensor and a target length, pads the tensor so that its length a multiple of the target length -
+    so that it can be split into equal parts.
+
+    Parameters
+    ----------
+    tensor : torch.tensor
+        The tensor to pad
+    target_length : int
+        The target length to pad the tensor to
+
+    Returns
+    -------
+    torch.tensor
+        The padded tensor
+    '''
+    
+    _, length = tensor.shape
+    if length % target_length != 0:
+        # Calculate padding needed
+        padding_needed = target_length - (length % target_length)
+        # Pad the tensor
+        tensor = torch.nn.functional.pad(tensor, (0, padding_needed))
+    return tensor
+
+def load_file(file, target_length=80):
+    '''
+    Given a file path, loads the file, creates a mel spectrogram, pads it to be cleanly divisible by target_length,
+    and splits it into chunks of target_length.
+
+    Parameters
+    ----------
+    file : str
+        The file path to load
+    target_length : int
+        The target length to split the tensor into
+
+    Returns
+    -------
+    tuple of torch.tensor
+        A tuple of tensors, each of length target_length
+    '''
+
+    # load wav file
+    waveform, _ = torchaudio.load(file, normalize=True)
+    waveform = waveform.squeeze(0)
+
+    # create mel spectrogram
+    mel_specgram = transform(waveform)
+
+    # pad tensor so it's cleanly divisible by target_length
+    padded_tensor = pad_tensor(mel_specgram, target_length)
+
+    # return the tensor, split into target_length chunks
+    return padded_tensor.split(target_length, dim=1)
+
+
+def load_dataset(meta_file, 
+                 target_length=80, 
+                 batch_size=32,
+                 files_to_load='all'):
+    '''
+    Given a meta file, loads in the dataset.
+
+    Parameters
+    ----------
+    meta_file : str
+        The meta file to load
+    target_length : int
+        The target length to split the tensor into
+    batch_size : int
+        The batch size to use for the DataLoader
+    files_to_load : int or 'all'
+        The number of files to load. If 'all', all files are loaded.
+
+    Returns
+    -------
+    tuple of torch.tensor
+        A tuple of tensors - the positives and negatives
+    '''
+
+    positives = []
+    negatives = []
+
+    # Load the meta file
+    meta = pd.read_csv(meta_file)
+
+    # iterate through the files
+    for i, row in enumerate(meta.itertuples()):
+        
+        # If we're only loading a subset of the files, check if we've loaded enough
+        if files_to_load != 'all' and i >= files_to_load:
+            break
+
+        # Load the file
+        file_tensors = load_file(os.path.join(data_dir, row.file), target_length=target_length)
+
+        # Add the tensors to the appropriate list
+        for tensor in file_tensors:
+            if row.numeric_label == 1:
+                positives.append(tensor)
+            else:
+                negatives.append(tensor)
+
+    # Create labels
+    positive_labels = torch.ones(len(positives))
+    negative_labels = torch.zeros(len(negatives))
+
+    # Combine the inputs
+    data = torch.cat((torch.stack(positives), torch.stack(negatives)), dim=0)
+    # Combine the labels
+    labels = torch.cat((positive_labels, negative_labels), dim=0)
+
+    # Create a dataset
+    dataset = TensorDataset(data, labels)
+
+    # Create a dataloader
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    # return the dataloader
+    return dataloader
